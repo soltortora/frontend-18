cf42ff158a8ea2074c3bc6fc90d228b3dca7c5c3
soltortora <soltortora12@gmail.com>
2023-06-13 12:39:07 -0300

    segundocommit

---
+++ Clase 1 07-06/script.js
@@ -0,0 +1,33 @@
+var nombre = 'Sol';
+var nombre = 'Valeria';
+
+
+let apellido = 'Ferrer';
+const mensaje = 'mensaje de prueba';
+
+apellido = 'Petriella';
+
+//nombres de variable empiezan en minuscula. nombreEmpleado, que empieza con mayucula
+//variables pueden cambiar. mensaje no se puede cambiar el valor por que es una constante, en cambio apellido si mensaje = 'hola, bienvenide'; a las constantes no se les puede cambiar el valor
+
+console.log (apellido)
+
+let name = prompt ('Ingresa tu nombre', 'nombre')
+
+let lastName = prompt ('Ingresa tu apellido', 'apellido')
+let age = prompt ('Ingresa tu edad', 'edad')
+
+console.log ('nombre', name);
+console.log ('apellido', lastName);
+console.log ('edad', age)
+
+alert('Hola ${name}') //le falta comillas invertidas, no las encontre :(
+
+let primerNumero = prompt('ingrese un numero', '')
+let segundoNumero =prompt('ingrese un numero para ser sumado', '')
+
+alert ('El resultado es ${resultado}') //falta comillas invertidas 
+
+
+
+
